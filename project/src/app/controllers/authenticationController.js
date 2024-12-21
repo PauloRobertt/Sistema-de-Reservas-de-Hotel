137caede933231ece3d2802992bc545321b11a52
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-12-21 15:02:28 -0300

    feat(back-end): adicionado controller e service para autenticação

---
+++ project/src/app/controllers/authenticationController.js
@@ -0,0 +1,25 @@
+import service from "../service/authenticationService.js";
+
+class authenticationController {
+    
+    async login(req, res){
+        try {
+            const result = await service.login(req.body);
+            res.status(200).json(result);
+        } catch (error) {
+            return res.status(500).json({message: `${error.message}`});
+        }
+    }
+
+    async register(req, res) {
+        try {
+            const result = await service.register(req.body);
+            return res.status(200).json(result);
+        }
+        catch (error) {
+            return res.status(500).json({message: `${error.message}`});
+        }
+    }
+}
+
+export default new authenticationController();
---
+++ project/src/app/service/authenticationService.js
@@ -0,0 +1,75 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import repository from '../repository/usuarioRepository.js';
+
+class authenticationService {
+    
+    async login(user) {
+        try {
+            const { email, senha } = user;
+            const usuarioExistente = await repository.findByEmail(email);
+            const acesso = 'Autorizado';
+
+            if (!email || !senha) {
+                throw new Error('Todos os campos são obrigatórios!');
+            }
+            
+            if (!usuarioExistente) {
+                throw new Error('Email não existe!')
+            }
+
+            if (senha.length < 8 || senha.length > 32) {
+                throw new Error('A senha deve ter no mínimo 8 caracteres e no maximo 32!');
+            }
+            
+            const passwordCompare = await bcrypt.compare(senha, usuarioExistente.senha);
+            if (usuarioExistente.email !== email || !passwordCompare) {
+                throw new Error('Email ou senha inválidos!')
+            }
+
+            return acesso;
+
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async register(user) {
+        try {
+            const { nome, email, senha, tipo } = user;
+
+            const salt = await bcrypt.genSalt(10);
+            const senhaHash = await bcrypt.hash(senha, salt);
+
+            if (!nome || !email || !senha) {
+                throw new Error('Todos os campos são obrigatórios!');
+            }
+
+            const usuarioExistente = await repository.findByEmail(email);
+
+            if (usuarioExistente !== null) {
+                throw new Error('Email já esta em uso!');
+            }
+
+            if (senha.length < 8 || senha.length > 32) {
+                throw new Error('A senha deve ter no mínimo 8 caracteres e no maximo 32!');
+            }
+
+            if (!senha.match(/[a-z]/g) || !senha.match(/[A-Z]/g) || !senha.match(/[\W|_]/g)) {
+                throw new Error('A senha deve conter letras maiusculas, minusculas e caracteres especiais');
+            }
+
+            return await repository.createUser({
+                nome: nome,
+                email: email,
+                senha: senhaHash,
+                tipo: tipo
+            });
+        }
+        catch (error) {
+            throw new Error(error.message);
+        }
+    }
+}
+
+export default new authenticationService();
