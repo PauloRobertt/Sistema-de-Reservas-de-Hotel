f841b0f18a3d763c221e34addaadf538d1761a0d
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-12-21 15:02:50 -0300

    feat(back-end): adicionadas rotas de autenticação ao router

--- project/src/router.js
+++ project/src/router.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import usuarioController from './app/controllers/usuarioController.js';
+import authenticationController from './app/controllers/authenticationController.js';
 const router = express.Router();
 
+//Rotas CRUD Usuario
 router.get('/usuario', usuarioController.findAll);
 router.get('/usuario/:id', usuarioController.findById);
 router.post('/usuario', usuarioController.createUser);
 router.put('/usuario/:id', usuarioController.editUser);
 router.delete('/usuario/:id', usuarioController.deleteUser);
+
+router.post('/auth/login', authenticationController.login);
+router.post('/auth/register', authenticationController.register);
 
 export default router;
