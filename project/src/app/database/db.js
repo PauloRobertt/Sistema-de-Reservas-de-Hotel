d7c4046ba7779f28050af988c66ac800804199db
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-12-28 16:54:29 -0300

    chore(database): integrar variáveis de ambiente na configuração

--- project/src/app/database/db.js
+++ project/src/app/database/db.js
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize';
+import 'dotenv/config';
 
-const sequelize = new Sequelize('Sistema_de_Reserva', 'postgres', 'root', {
-    host: 'localhost',
+const sequelize = new Sequelize('Sistema_de_Reserva', process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'postgres',
 });
 
