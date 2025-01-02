75b4de8c6c58010d0b2dc41f030440c8d3a4f636
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-01-02 01:27:38 -0300

    feat(back-end): melhorias no TokenService
    - Adicionado 'email' e 'role' ao token gerado
    - Token agora possui validade de 1 hora
    - Incluída verificação do token decodificado para acessar as rotas

--- project/src/app/security/TokenService.js
+++ project/src/app/security/TokenService.js
@@ -4,7 +4,15 @@ import jwt from 'jsonwebtoken';
 class TokenService {
     generateToken(user) {
         const secret = process.env.SECRET;
-        const token = jwt.sign({ id: user.id }, secret,);
+        const token = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                role: user.role
+            },
+            secret,
+            { expiresIn: '1h' }
+        );
 
         return token;
     }
@@ -12,20 +20,26 @@ class TokenService {
     checkToken(req, res, next) {
         try {
             const secret = process.env.SECRET;
-            
+
             const authHeader = req.headers['authorization'];
             const token = authHeader && authHeader.split(' ')[1];
 
             if (!token) {
-                res.status(404).json({msg: 'Acesso Negado!'});
+                res.status(403).json({ msg: 'Token não encontrado!' });
             }
 
-            jwt.verify(token, secret);
+            const decode = jwt.verify(token, secret);
+
+            req.user = {
+                id: decode.id, 
+                email: decode.email, 
+                role: decode.role
+            };
 
             next();
 
         } catch (error) {
-            res.status(404).json({msg: 'Token Invalido!'});
+            res.status(404).json({ msg: 'Token Invalido!' });
         }
     }
 };
